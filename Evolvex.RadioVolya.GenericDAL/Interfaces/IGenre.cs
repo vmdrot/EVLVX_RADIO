1f404f4b8dfc42265ae6d03f61876eba26c9f94f
jefe <jefe@f95ddeb0-f21f-465a-bb26-d024b75bd216>
2012-11-18 21:57:00 +0000

    + generic DAL (to strip off from concrete RDMBS specifics) - started, in progress

---
+++ Evolvex.RadioVolya.GenericDAL/Interfaces/IGenre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Evolvex.RadioVolya.GenericDAL.Interfaces
+{
+    public interface IGenre
+    {
+        int ID { get; set; }
+        int? ParentID { get; set; }
+        string Name { get; set; }
+    }
+}
---
+++ Evolvex.RadioVolya.GenericDAL/Interfaces/ITrackInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Evolvex.RadioVolya.GenericDAL.Interfaces
+{
+    public interface ITrackInfo
+    {
+        int ID { get; set; }
+        int Title { get; set; }
+        IList<IArtistInfo> Artists { get; set; }
+        IList<IGenre> Genres { get; set; }
+
+
+    }
+}
