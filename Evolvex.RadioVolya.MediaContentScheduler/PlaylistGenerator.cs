b5f4c35fe37ba53ad8f717c9f339c49a282dcb09
jefe <vmdrot@users.noreply.github.com>
2012-11-22 13:51:06 +0000

    + entity framework, ddl for ms sql, shuffler (proper) - in progress

--- Evolvex.RadioVolya.MediaContentScheduler/PlaylistGenerator.cs
+++ Evolvex.RadioVolya.MediaContentScheduler/PlaylistGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Practices.Unity;
+using Evolvex.RadioVolya.GenericDAL.Interfaces;
 
 namespace Evolvex.RadioVolya.MediaContentScheduler
 {
@@ -10,11 +11,12 @@ namespace Evolvex.RadioVolya.MediaContentScheduler
     {
         #region field(s)
         private int _channelId;
-        private RandomTrackDispenser _dispenser;
+        private IRandomTrackDispenser _dispenser;
 
         #endregion
         public PlaylistGenerator(IUnityContainer cont)
         {
+
         }
 
         public PlaylistGenerator(int channelId, DateTime airStart, DateTime airEnd)
