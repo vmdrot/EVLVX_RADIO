2090144820bca3ca7bfb3d34691869fffcb9ca19
jefe <vmdrot@users.noreply.github.com>
2012-11-25 22:11:24 +0000

    + interfaces-based stage - in progress...

--- Evolvex.RadioVolya.MediaContentScheduler/RandomTrackDispenser.cs
+++ Evolvex.RadioVolya.MediaContentScheduler/RandomTrackDispenser.cs
@@ -2,28 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Evolvex.RadioVolya.Core.DIServiceContracts;
+using Microsoft.Practices.Unity;
 
 namespace Evolvex.RadioVolya.MediaContentScheduler
 {
-    public class RandomTrackDispenser
+    public class RandomTrackDispenser : IRandomTrackDispenser
     {
         #region field(s)
+        private IPlayListGenerationParams _params;
         private int _channelId;
         private List<int> _alreadyTried;
+        private List<int> _wholePopulation;
+        private ITrackPopulationLister _trackPopulationLister;
+        private Random _rnd;
         #endregion
 
-        #region cctor(s)
-        public RandomTrackDispenser(int channelId)
+        #region prop(s)
+        private List<int> AvailableTrackIDs
         {
-            this._channelId = channelId;
-            this._alreadyTried = new List<int>();
+            get
+            {
+                if (this._wholePopulation == null)
+                    this._wholePopulation = this._trackPopulationLister.List(this._params.ChannelID);
+                return this._wholePopulation;
+            }
         }
+        #endregion
+        #region cctor(s)
+        public RandomTrackDispenser(IUnityContainer cont)
+        {
+            _params = cont.Resolve<IPlayListGenerationParams>();
+            _trackPopulationLister = cont.Resolve<ITrackPopulationLister>();
+            this._alreadyTried = new List<int>();
+            this._wholePopulation = _trackPopulationLister.List(this._params.ChannelID);
+            this._rnd = new Random();
+        }
+
+        //public RandomTrackDispenser(int channelId)
+        //{
+        //    this._channelId = channelId;
+        //    this._alreadyTried = new List<int>();
+        //}
         #endregion
 
         private int GenerateRandomID()
         {
-            return 0;//todo;
+            int idx = this._rnd.Next(this.AvailableTrackIDs.Count - 1);
+            return this.AvailableTrackIDs[idx];
         }
+
         public int Next()
         {
             int rslt = 0;
