d5be5a677a4774398c15944e03ed3278a6602a17
jefe <jefe@f95ddeb0-f21f-465a-bb26-d024b75bd216>
2012-11-27 17:07:28 +0000

    + interfaces-based stage - in progress...

--- Evolvex.RadioVolya.Core/DIServiceContracts/IRandomTrackDispenser.cs
+++ Evolvex.RadioVolya.Core/DIServiceContracts/IRandomTrackDispenser.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Evolvex.RadioVolya.GenericDAL.Interfaces
+namespace Evolvex.RadioVolya.Core.DIServiceContracts
 {
     public interface IRandomTrackDispenser
     {
-
+        int Next();
     }
 }
---
+++ Evolvex.RadioVolya.Core/DIServiceContracts/ITrackPopulationLister.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Evolvex.RadioVolya.Core.DIServiceContracts
+{
+    public interface ITrackPopulationLister
+    {
+        List<int> List(int channelId);
+    }
+}
