620ff42e0ac5c7aa739008b442a71cdad8c5eb98
jefe <jefe@f95ddeb0-f21f-465a-bb26-d024b75bd216>
2012-11-30 21:38:32 +0000

    +* in progress

---
+++ Evolvex.RadioVolya.YouthChannelSimpleScheduler/YouthChannelGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Evolvex.RadioVolya.Core.DIServiceContracts;
+using Evolvex.RadioVolya.GenericDAL.Interfaces;
+
+namespace Evolvex.RadioVolya.YouthChannelSimpleScheduler
+{
+    public class YouthChannelGenerator: IPlaylistGenerator
+    {
+        #region IPlaylistGenerator Members
+
+        public List<ITrackInfo> Generate()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
